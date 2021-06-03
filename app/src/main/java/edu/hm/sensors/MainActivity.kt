49ad00c65808d1ebc4b2f5e73a00745c7865179b
TobiP3005 <65250269+TobiP3005@users.noreply.github.com>
2021-06-03 19:14:37 +0200

    Data binding implemented

--- app/src/main/java/edu/hm/sensors/MainActivity.kt
+++ app/src/main/java/edu/hm/sensors/MainActivity.kt
@@ -2,10 +2,17 @@ package edu.hm.sensors
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.databinding.DataBindingUtil
+import edu.hm.sensors.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        binding.mainActivity = this
     }
 }
